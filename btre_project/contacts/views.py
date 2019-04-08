3a338a0cd0252d91106b80e03da047842c97f26b
ccastell18 <c.castellano18@gmail.com>
2019-04-07 20:00:39 -0500

    inquery email working

--- btre_project/contacts/views.py
+++ btre_project/contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Contact
+from django.core.mail import send_mail
 
 
 def contact(request):
@@ -13,11 +14,28 @@ def contact(request):
     message = request.POST['message']
     user_id = request.POST['user_id']
     realtor_email = request.POST['realtor_email']
+
+    #Check i user has made an inquery
+    if request.user.is_authenticated:
+      user_id= request.user.id 
+      has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+      if has_contacted:
+        messages.error(request, 'You have already made an inquery for this listing.')
+        return redirect('listings')
     
     contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id )
 
     contact.save()
 
+    #send mail
+    send_mail(
+      'Property Listing Inquiry',
+      'There has been an inquery for ' + listing + '.  Sign into the admin panel for more information.',
+      'chris.cas1880@gmail.com',
+      [realtor_email],
+      fail_silently=False
+    )
+
     messages.success(request, 'Your request has been submitted. A realtor will get back to you soon.')
 
-    return redirect('listing/'+listing_id)
+    return redirect('listings')
