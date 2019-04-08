8ff46434c3ee44a9d9e09b9ded39d0a394b8c331
ccastell18 <c.castellano18@gmail.com>
2019-04-07 20:24:19 -0500

    dynamic dashboard. project done.

--- btre_project/accounts/views.py
+++ btre_project/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
+from contacts.models import Contact
+
 
 def register(request):
     if request.method == "POST":
@@ -57,7 +59,11 @@ def login(request):
         return render(request, 'accounts/login.html')
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+    context = {
+        'contacts': user_contacts
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 def logout(request):
     if request.method == "POST":
--- btre_project/contacts/views.py
+++ btre_project/contacts/views.py
@@ -20,7 +20,7 @@ def contact(request):
       user_id= request.user.id 
       has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
       if has_contacted:
-        messages.error(request, 'You have already made an inquery for this listing.')
+        messages.error(request, 'You have already made an inquiry for this listing.')
         return redirect('listings')
     
     contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id )
@@ -30,8 +30,8 @@ def contact(request):
     #send mail
     send_mail(
       'Property Listing Inquiry',
-      'There has been an inquery for ' + listing + '.  Sign into the admin panel for more information.',
-      'chris.cas1880@gmail.com',
+      'There has been an inquiry for ' + listing + '.  Sign into the admin panel for more information.',
+      '',
       [realtor_email],
       fail_silently=False
     )
